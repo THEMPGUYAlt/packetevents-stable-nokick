4354d51cd6e434848673a78b23e90534d7f21a09
THEMPGUY <realmsnetwork4@gmail.com>
2026-05-17 22:58:01 -0400

    Update PlayerListMixin.java

--- fabric-official/src/main/java/io/github/retrooper/packetevents/mixin/PlayerListMixin.java
+++ fabric-official/src/main/java/io/github/retrooper/packetevents/mixin/PlayerListMixin.java
@@ -84,8 +84,9 @@ public class PlayerListMixin {
             // Check if it's a fake connection
             if (!FakeChannelUtil.isFakeChannel(channelObj) &&
                     (!api.isTerminated() || api.getSettings().isKickIfTerminated())) {
-                // Kick the player if they're not a fake player
-                player.connection.disconnect(Component.literal("PacketEvents failed to inject into a channel."));
+                // Kick the player if they're not a fake player --DISABLED
+                //player.connection.disconnect(Component.literal("PacketEvents failed to inject into a channel."));
+                return;
             }
             return;
         }
